126df2a4cb717136cf6b74a80d764aecf7f3af9f
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-06-02 10:09:37 +0300

    Start

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def masking_cards(num_cards: [str]) -> [str]:
+    """Функция маскирует номер карты пользователя"""
+    if num_cards.isdigit() and len(num_cards) == 16:
+        return f"{num_cards[0:5]} {num_cards[5:7]}{"*" * 2} {"*" * 4} {num_cards[12:]}"
+    else:
+        return "Не верно введен номер карты"
+
+
+def check_mask(num_check: [str]) -> [str]:
+    """Функция маскирует номер счета"""
+    if num_check.isdigit() and len(num_check) == 20:
+        return f"{"*" * 2}{num_check[-4:]}"
+    else:
+        return "Не верно введен номер счета"
