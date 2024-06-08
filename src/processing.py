28346bd2e7e401e2c4d1be246b03159ae910d728
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-06-08 14:46:36 +0300

    add readme, upp_processing

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,6 @@ def filter_by_state(my_dict: list, state_='EXECUTED') -> list:
 
 
 def sort_by_date(my_dict: list, direction: bool = True) -> list:
-    """Функция"""
+    """Функция для сортировки словарей по дате"""
     sort_by_date_ = sorted(my_dict, key=lambda my_dict: my_dict.get("date"), reverse=direction)
     return sort_by_date_
