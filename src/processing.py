c0f77de55e28ae5a23e557a946616e51af38dddf
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-06-08 14:16:57 +0300

    add 2 func

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def sort_to_state(my_dict: list, state_='EXECUTED') -> list:
+def filter_by_state(my_dict: list, state_='EXECUTED') -> list:
     """Функция сортирует списки словарей по второму заданному аргументу"""
     return_list = []
     for i in my_dict:
@@ -7,7 +7,7 @@ def sort_to_state(my_dict: list, state_='EXECUTED') -> list:
     return return_list
 
 
-print(sort_to_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+def sort_by_date(my_dict: list, direction: bool = True) -> list:
+    """Функция"""
+    sort_by_date_ = sorted(my_dict, key=lambda my_dict: my_dict.get("date"), reverse=direction)
+    return sort_by_date_
