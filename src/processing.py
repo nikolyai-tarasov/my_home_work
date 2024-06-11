1493713638ac08b111b1e65d60ead2baaf49e8a5
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-06-11 23:33:29 +0300

    tested processing

--- src/processing.py
+++ src/processing.py
@@ -4,10 +4,18 @@ def filter_by_state(my_dict: list, state_: str = "EXECUTED") -> list:
     for i in my_dict:
         if i["state"] == state_:
             return_list.append(i)
+        elif state_ != "EXECUTED" and state_ != "CANCELED":
+            return "Введите корректный 2 аргумент"
+
     return return_list
 
 
 def sort_by_date(my_dict: list, direction: bool = True) -> list:
     """Функция для сортировки словарей по дате"""
-    sort_by_date_ = sorted(my_dict, key=lambda my_dict: my_dict.get("date"), reverse=direction)
-    return sort_by_date_
+    if isinstance(my_dict, list) and direction != bool:
+        sort_by_date_ = sorted(my_dict, key=lambda my_dict: my_dict.get("date"), reverse=direction)
+        return sort_by_date_
+    return "Вы ввели не соответствующие данные"
+
+
+
