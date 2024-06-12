cf8982ad0eff6aaeb83e01ddc918dd2dcc4bf87e
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-06-12 10:04:42 +0300

    pakeg tests

--- tests/test_home_work.py
+++ tests/test_home_work.py
@@ -1,6 +1,8 @@
 import pytest
-from src.masks import masking_cards, check_mask
+
+from src.masks import check_mask, masking_cards
 from src.processing import filter_by_state, sort_by_date
+from src.widget import info_cart, transformation_date
 
 
 # тесты для маски номера карты
@@ -14,10 +16,13 @@ def test_mask_card(num_mask):
         masking_cards(1234564789567465)
 
 
-@pytest.mark.parametrize('value, expected', [
-    ("123456478956746", "Не верно введен номер карты"),
-    ("123456dsdf674653", "Не верно введен номер карты"),
-])
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("123456478956746", "Не верно введен номер карты"),
+        ("123456dsdf674653", "Не верно введен номер карты"),
+    ],
+)
 def test_other(value, expected):
     assert masking_cards(value) == expected
 
@@ -33,34 +38,65 @@ def test_check_mask(tst_check_mask):
         check_mask(1234564789567465)
 
 
-@pytest.mark.parametrize('value, expected', [
-    ("1234231534264576489", "Не верно введен номер счета"),
-    ("12342315fgdh45764892", "Не верно введен номер счета"),
-])
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("1234231534264576489", "Не верно введен номер счета"),
+        ("12342315fgdh45764892", "Не верно введен номер счета"),
+    ],
+)
 def test_check_mask_other(value, expected):
     assert check_mask(value) == expected
 
 
 def test_filter(proces_test):
-    assert filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                           ) == proces_test
+    assert (
+            filter_by_state(
+                [
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                ],
+            )
+            == proces_test
+    )
 
 
 def test_filter():
-    assert filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                           "sdfxz") == "Введите корректный 2 аргумент"
+    assert (
+            filter_by_state(
+                [
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                ],
+                "sdfxz",
+            )
+            == "Введите корректный 2 аргумент"
+    )
 
 
 def test_sort_date(sort_date):
-    assert sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]) == sort_date
+    assert (
+            sort_by_date(
+                [
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                ]
+            )
+            == sort_date
+    )
 
 
+def test_info_cart():
+    assert info_cart("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+    assert info_cart("") == " Не верно введен номер карты"
+
+
+def test_trans_date():
+    assert transformation_date("2018-07-11T02:26:18.671407") == "11.07.2018"
+    assert transformation_date("") == "Введена не корректная дата"
