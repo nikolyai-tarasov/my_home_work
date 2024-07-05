834804a55ec9d335730bb92f469a2f4f07fe1b1a
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-07-05 20:16:48 +0300

    upp src

--- src/external_api.py
+++ src/external_api.py
@@ -3,35 +3,37 @@ import os
 import requests
 from src.utils import read_file
 from dotenv import load_dotenv
+
 load_dotenv('.env')
 
 API_KEY = os.getenv('API_KEY')
 
+
 def currency_conversion(transaction: list) -> float:
+    """Функция конвертации валюты"""
     count_sum_amount = 0
-    for i in transaction:
-        if i['operationAmount']['currency']['code'] == "USD":
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={i['operationAmount']['amount']}"
-            payload = {}
-            headers = {
-                "apikey": API_KEY
-            }
-            response = requests.request("GET", url, headers=headers, data=payload)
-            status_code = response.status_code
-            result = response.json()
-            count_sum_amount += result["result"]
-        elif i['operationAmount']['currency']['code'] == "EUR":
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount={i['operationAmount']['amount']}"
-            payload = {}
-            headers = {
-                "apikey": API_KEY
-            }
-            response = requests.request("GET", url, headers=headers, data=payload)
-            status_code = response.status_code
-            result = response.json()
-            count_sum_amount += result['result']
-        else:
-            count_sum_amount += float(i['operationAmount']['amount'])
+    if transaction['operationAmount']['currency']['code'] == "USD":
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={transaction['operationAmount']['amount']}"
+        payload = {}
+        headers = {
+            "apikey": API_KEY
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        status_code = response.status_code
+        result = response.json()
+        count_sum_amount += result["result"]
+    elif transaction['operationAmount']['currency']['code'] == "EUR":
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount={transaction['operationAmount']['amount']}"
+        payload = {}
+        headers = {
+            "apikey": API_KEY
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        status_code = response.status_code
+        result = response.json()
+        count_sum_amount += result['result']
+    else:
+        count_sum_amount += float(transaction['operationAmount']['amount'])
 
     return count_sum_amount
 
