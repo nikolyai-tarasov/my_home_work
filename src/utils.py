5ed96cc9adead3c02db64a13dfccea3a33098098
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-07-05 12:41:09 +0300

    utils

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import json
+
+
+def read_file(filename: str = None) -> list:
+    """Функция считывающая информацию JSON формата с заданного файла"""
+    with open(filename, encoding="utf-8") as file:
+        reading = json.load(file)
+        if type(reading) is not list or len(reading) == 0 or filename is None:
+            return []
+    return reading
+
+
+transaction = read_file("../data/operations.json")
+print(transaction)
