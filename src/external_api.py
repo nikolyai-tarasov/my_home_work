b65d30e748fd25c9f0576886bfead4f8654599e0
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-07-08 20:41:43 +0300

    upp_4

--- src/external_api.py
+++ src/external_api.py
@@ -13,25 +13,24 @@ API_KEY = os.getenv("API_KEY")
 def currency_conversion(transaction: list) -> float:
     """Функция конвертации валюты"""
     count_sum_amount = 0
-    url = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount="
+    url_usd = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount="
+    url_eur = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount="
     if transaction[0]["operationAmount"]["currency"]["code"] == "USD":
-        url_1 = f"{url}{transaction[0]["operationAmount"]['amount']}"
+        url_1 = f"{url_usd}{transaction[0]["operationAmount"]['amount']}"
         payload = {}
         headers = {"apikey": API_KEY}
         response = requests.request("GET", url_1, headers=headers, data=payload)
         status_code = response.status_code
         result = response.json()
-        count_sum_amount += result["result"]
-        return count_sum_amount
+        return result['result']
     elif transaction[0]["operationAmount"]["currency"]["code"] == "EUR":
-        url_1 = f"{url}{transaction[0]["operationAmount"]["amount"]}"
+        url_1 = f"{url_eur}{transaction[0]["operationAmount"]["amount"]}"
         payload = {}
         headers = {"apikey": API_KEY}
         response = requests.request("GET", url_1, headers=headers, data=payload)
         status_code = response.status_code
         result = response.json()
-        count_sum_amount += result["result"]
-        return count_sum_amount
+        return result['result']
     else:
         count_sum_amount += float(transaction[0]["operationAmount"]["amount"])
 
@@ -39,5 +38,5 @@ def currency_conversion(transaction: list) -> float:
 
 
 if __name__ == "__main__":
-    res_func = read_file("../data/operations.json")
+    res_func = read_file("../data/tests_operations.json")
     print(currency_conversion(res_func))
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -6,9 +6,9 @@ from src.external_api import currency_conversion, read_file
 @patch("requests.request")
 def test_currency_conversion(mock_get):
     mock_get.return_value.json.return_value = {"operationAmount": {"amount": "31957.58"}}
-    assert currency_conversion(read_file("../data/operations.json")) == 31957.58
+    assert currency_conversion(read_file("../data/tests_operations.json")) == 2800296.753175
     mock_get.assert_called_once_with(
-        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=31957.58"
+        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount="
     )
 
 
