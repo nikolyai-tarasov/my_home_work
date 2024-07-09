15151c160aa52b6a18f3f920261f5f3fb157ba81
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-07-09 20:42:10 +0300

    upp final

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -5,10 +5,11 @@ from src.external_api import currency_conversion, read_file
 
 @patch("requests.request")
 def test_currency_conversion(mock_get):
-    mock_get.return_value.json.return_value = {"operationAmount": {"amount": "31957.58"}}
-    assert currency_conversion(read_file("../data/tests_operations.json")) == 2800296.753175
-    mock_get.assert_called_once_with(
-        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount="
+    mock_get.return_value.json.return_value = {'result': 2780295.878029}
+    assert currency_conversion(read_file("../data/tests_operations.json")) == 2780295.878029
+    mock_get.assert_called_once_with("GET",
+        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=31957.58",
+        headers={'apikey': 'GOPnsVnjeGo3YO9JctoMd6ZfA4rjYTBW'}, data={}
     )
 
 
