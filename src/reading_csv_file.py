3206c60083e9ea15906978d23b7d0d62a2458454
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-07-12 18:59:21 +0300

    upp for read csv and excel file

--- src/reading_csv_file.py
+++ src/reading_csv_file.py
@@ -1,4 +1,19 @@
-import pandas as pd
+import csv
 
-product = pd.read_csv("../data/data_csv-/transactions.csv")
-print(product)
+
+def read_csv(path_file):
+    with open(path_file, "r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=";")
+        header = next(reader)
+        result = []
+        for row in reader:
+            row_dict = dict()
+            for ind, itm in enumerate(header):
+                row_dict[itm] = row[ind]
+            result.append(row_dict)
+        return result
+
+
+# "../data/data_csv-/transactions.csv"
+if __name__ == "__main__":
+    print(read_csv("../data/data_csv-/transactions.csv"))
